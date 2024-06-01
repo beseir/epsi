03a667a4da7c756504dcecff56ae709fe5cf5ea7
vanya <beseir28@gmail.com>
2024-06-01 17:50:02 +0300

    telegram test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import 'katex/dist/katex.min.css';
 import Latex from 'react-latex-next';
-
+import {useEffect} from "react";
+const tg = window.Telegram.WebApp;
 function App() {
-
+    useEffect(() =>{
+        tg.ready();
+    }, [])
+    const tex = "$$latex = {vanya}^{beloded}$$";
     return (
     <div className="App">
-
-        <Latex>{"$$latex = {vanya}^{beloded}$$"}</Latex>
+        Привет {tg.initDataUnsafe?.user?.username}
+        <Latex>{tex}</Latex>
 
     </div>
   );
