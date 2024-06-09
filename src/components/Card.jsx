875396943da1c0606de7f6acfe348dc3270689ec
vanya <beseir28@gmail.com>
2024-06-10 00:04:48 +0300

    card markup

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,21 +9,23 @@ export const Card = () => {
         const clientY = e.type === 'touchstart' ? e.touches[0].clientY : e.clientY;
         const x = clientX - card.left - card.width / 2;
         const y = clientY - card.top - card.height / 2;
-        setRotation({ x: y / 13, y: -x / 13 });
+        setRotation({ x: y / 14, y: -x / 14 });
     };
 
     const handleMouseUp = () => {
         setRotation({ x: 0, y: 0 });
     };
     return (
-        <div className={"card"}  onMouseDown={handleMouseDown}
+        <div className={"card"} onMouseDown={handleMouseDown}
              onMouseUp={handleMouseUp}
              onTouchStart={handleMouseDown}
              onTouchEnd={handleMouseUp}
              style={{
                  transform: `rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`,
-             }} >
-            SomeCard
+             }}>
+
+            <img className={"card-image"} src={"images/test.jpg"} alt={"image"}/>
+            <h2>CARD NAME</h2>
         </div>
     )
 }
