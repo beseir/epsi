8b4f276036e4faf99d3b23fdd1384d030805d344
vanya <beseir28@gmail.com>
2024-06-09 22:16:45 +0300

    card animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,25 @@
 import "../styles/Card.css"
+import {useState} from "react";
 export const Card = () => {
+    const [rotation, setRotation] = useState({ x: 0, y: 0 });
+
+    const handleMouseDown = (e) => {
+        const card = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - card.left - card.width / 2;
+        const y = e.clientY - card.top - card.height / 2;
+        setRotation({ x: y / 15, y: -x / 15 });
+    };
+
+    const handleMouseUp = () => {
+        setRotation({ x: 0, y: 0 });
+    };
     return (
-        <div className={"card"} >
+        <div className={"card"}  onMouseDown={handleMouseDown}
+             onMouseUp={handleMouseUp}
+
+             style={{
+                 transform: `rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`,
+             }} >
             SomeCard
         </div>
     )
