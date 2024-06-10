bafdc53ef1fc71ae7696b2d4981e8b62894791f3
vanya <beseir28@gmail.com>
2024-06-10 12:59:27 +0300

    axios remove

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,43 +1,16 @@
-import axios from 'axios';
 import {Card} from "./Card";
 import '../styles/App.css';
 import {NavPage} from "./NavPage";
-import {useEffect, useState} from "react";
+import {useCallback, useState} from "react";
 const tg = window.Telegram.WebApp;
 
 
 export const Home = () => {
-    const [loading, setLoading] = useState(true);
-    const userId = "1"; // Уникальный идентификатор пользователя
     const [coins, setCoins] = useState(0);
 
-    useEffect(() => {
-        const fetchclicks = async () => {
-            try {
-                const response = await axios.get(`http://localhost:5000/api/clicks/${userId}`);
-                setCoins(response.data.clicks);
-            } catch (error) {
-                console.error('Error fetching clicks:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchclicks();
-    }, []);
-
-    const addCoins = async () => {
-        try {
-            const response = await axios.post(`http://localhost:5000/api/clicks/${userId}`, {
-                clicks: 98
-            });
-            setCoins(response.data.clicks);
-        } catch (error) {
-            console.error('Error adding clicks:', error);
-        }
-    };
-
-    if (loading) return <div>Loading...</div>;
+    const onCardClick = useCallback(() => {
+        setCoins(coins + 1);
+    }, [coins]);
 
 
     return (
@@ -45,7 +18,7 @@ export const Home = () => {
             <div className="App">
                 <h1 className={"coins-text"}>{coins}</h1>
                 <h4 className={"card-preview-text"}>Привет {tg.initDataUnsafe?.user?.username}, вот твоя карточка:</h4>
-                <Card onClick={addCoins}/>
+                <Card onClick={onCardClick}/>
             </div>
             <NavPage/>
         </>
