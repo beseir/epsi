0d44d0ddd70131a5a8461e3b5bc524e55ab6182a
vanya <beseir28@gmail.com>
2024-06-10 11:14:22 +0300

    added coins, clicker mechanic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import "../styles/Card.css"
 import {useState} from "react";
-export const Card = () => {
+export const Card = ({onClick}) => {
     const [rotation, setRotation] = useState({ x: 0, y: 0 });
 
     const handleMouseDown = (e) => {
@@ -9,23 +9,31 @@ export const Card = () => {
         const clientY = e.type === 'touchstart' ? e.touches[0].clientY : e.clientY;
         const x = clientX - card.left - card.width / 2;
         const y = clientY - card.top - card.height / 2;
+
         setRotation({ x: y / 14, y: -x / 14 });
+        onClick();
     };
 
     const handleMouseUp = () => {
         setRotation({ x: 0, y: 0 });
     };
     return (
-        <div className={"card"} onMouseDown={handleMouseDown}
+        <div className={"card"}
+             onMouseDown={handleMouseDown}
              onMouseUp={handleMouseUp}
              onTouchStart={handleMouseDown}
              onTouchEnd={handleMouseUp}
              style={{
                  transform: `rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`,
              }}>
-
+            <h1 className={"card-rare-level"}>S</h1>
             <img className={"card-image"} src={"images/test.jpg"} alt={"card"}/>
-            <h2>CARD NAME</h2>
+            <h2>SIMPLE CARD</h2>
+            <div className={"card-stats"}>
+                <p>LEVEL: 1</p>
+                <p>CLICK-ABILITY: 1</p>
+            </div>
+
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,20 @@
 import {Card} from "./Card";
 import '../styles/App.css';
 import {NavPage} from "./NavPage";
+import {useCallback, useState} from "react";
 const tg = window.Telegram.WebApp;
 export const Home = () => {
+    const [coins, setCoins] = useState(0);
+    const onCardClick = useCallback(() => {
+        setCoins(coins+1);
+    }, [coins]);
+
     return (
         <>
             <div className="App">
+                <h1 className={"coins-text"}>{coins}</h1>
                 <h4 className={"card-preview-text"}>Привет {tg.initDataUnsafe?.user?.username}, вот твоя карточка:</h4>
-                <Card/>
-
+                <Card onClick={onCardClick}/>
             </div>
             <NavPage/>
         </>
