f763c415ed0d5add5079cc65451ab9ac85b9b9eb
vanya <beseir28@gmail.com>
2024-06-15 17:30:11 +0300

    added caps

--- src/components/Cap.jsx
+++ src/components/Cap.jsx
@@ -1,6 +1,6 @@
-import "../styles/Card.css"
+import "../styles/Cap.css";
 import {useState} from "react";
-export const Card = () => {
+export const Cap = () => {
     const [rotation, setRotation] = useState({ x: 0, y: 0 });
     const handleMouseDown = (e) => {
         const card = e.currentTarget.getBoundingClientRect();
@@ -9,7 +9,7 @@ export const Card = () => {
         const x = clientX - card.left - card.width / 2;
         const y = clientY - card.top - card.height / 2;
 
-        setRotation({ x: y / 14, y: -x / 14 });
+        setRotation({ x: y / 6, y: -x / 6 });
 
     };
 
@@ -17,22 +17,14 @@ export const Card = () => {
         setRotation({ x: 0, y: 0 });
     };
     return (
-        <div className={"card"}
-             onMouseDown={handleMouseDown}
+        <div className={"cap"}  onMouseDown={handleMouseDown}
              onMouseUp={handleMouseUp}
              onTouchStart={handleMouseDown}
              onTouchEnd={handleMouseUp}
              style={{
                  transform: `rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`,
              }}>
-            <h1 className={"card-rare-level"}>S</h1>
-            <img className={"card-image"} src={"images/test.jpg"} alt={"card"}/>
-            <h2>SIMPLE CARD</h2>
-            <div className={"card-stats"}>
-                <p>LEVEL: 1</p>
-                <p>CLICK-ABILITY: 1</p>
-            </div>
-
+        <img className={"cap-image"} src={"./images/test.jpg"} alt={"cap"}/>
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
-import {Card} from "./Card";
 import '../styles/App.css';
 import {NavPage} from "./NavPage";
 import {useEffect, useState} from "react";
 
 import Flicking from "@egjs/react-flicking";
 import "@egjs/react-flicking/dist/flicking.css";
+import {Cap} from "./Cap";
 
 const tg = window.Telegram.WebApp;
 
@@ -22,9 +22,9 @@ export const Home = () => {
                 <h1 className={"coins-text"}>{coins}</h1>
                 <h4 className={"card-preview-text"}>Привет {tg.initDataUnsafe?.user?.username || "username"}, вот твоя коллекция:</h4>
                 <Flicking circular={false}>
-                    <div><Card/></div>
-                    <div><Card/></div>
-                    <div><Card/></div>
+                    <div><Cap/></div>
+                    <div><Cap/></div>
+                    <div><Cap/></div>
                 </Flicking>
 
 
